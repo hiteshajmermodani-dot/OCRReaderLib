3c1fdede9acf46685cd7e5bc48a08617291557fb
hiteshajmermodani-dot <hitesh.ajmer.modani@gmail.com>
2026-07-09 16:46:46 +0530

    Add files via upload

---
+++ OCRReader/Converters.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace OCRReader
+{
+    /// <summary>
+    /// Converts a boolean value to Visibility (inverted)
+    /// </summary>
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility)
+            {
+                return visibility == Visibility.Collapsed;
+            }
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Converts null value to Visibility (visible if null, collapsed if not null)
+    /// </summary>
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OCRReader/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using OCRReader.ViewModels;
+
+namespace OCRReader
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Set DataContext to OcrViewModel for MVVM data binding
+            this.DataContext = new OcrViewModel();
+        }
+    }
+}
